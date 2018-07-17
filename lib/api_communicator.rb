8a836833f8e5695bb61bc78dc09a33d3ab77090b
Ali Aziz <ali.aziz@flatironschool.com>
2018-07-17 18:30:58 +0100

    completed

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -6,27 +6,30 @@ def get_character_movies_from_api(character)
   #make the web request
   all_characters = RestClient.get('http://www.swapi.co/api/people/')
   character_hash = JSON.parse(all_characters)
-  
-  # iterate over the character hash to find the collection of `films` for the given
-  #   `character`
-  # collect those film API urls, make a web request to each URL to get the info
-  #  for that film
-  # return value of this method should be collection of info about each film.
-  #  i.e. an array of hashes in which each hash reps a given film
-  # this collection will be the argument given to `parse_character_movies`
-  #  and that method will do some nice presentation stuff: puts out a list
-  #  of movies by title. play around with puts out other info about a given film.
+  target_character = character_hash['results'].find do |result|
+    result['name'] == character
+  end
+  target_character['films']
 end
 
+
 def parse_character_movies(films_hash)
+  films_hash.map do |film|
+    film_info = RestClient.get(film)
+    film_info = JSON.parse(film_info)
+    film_info['title']
+  end
+
   # some iteration magic and puts out the movies in a nice list
 end
+
 
 def show_character_movies(character)
   films_hash = get_character_movies_from_api(character)
   parse_character_movies(films_hash)
 end
 
+show_character_movies("Luke Skywalker")
 ## BONUS
 
 # that `get_character_movies_from_api` method is probably pretty long. Does it do more than one job?
